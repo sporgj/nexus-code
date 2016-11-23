93e0ea95d992d895eac5e80489dc02584ab753e4
briand <jbriand@cs.pitt.edu>
2016-11-23 17:21:54 +0000

    updated gen_file to flush file on AFS

--- bench/gen_file.c
+++ bench/gen_file.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -27,6 +28,7 @@ static void usage(char* prog) {
     printf("usage: %s size{K,M,G} [repeat]\n", prog);
 }
 
+int is_afs_env = 0;
 typedef enum { WRITE, READ } io_op_t;
 
 inline clock_t file_io(io_op_t op, char* fname, int64_t total_size) {
@@ -60,14 +62,28 @@ inline clock_t file_io(io_op_t op, char* fname, int64_t total_size) {
     return clock() - diff;
 }
 
+const char * flush_afs_cmd = "fs flush -p ./%s";
+
 static void gen_file(int64_t total_size, int repeat) {
-    char fname[20];
+    int status;
+    char fname[20], cmd[100];
     clock_t diff, total_write = 0, total_read = 0;
     double avg_read, avg_write;
 
     snprintf(fname, sizeof(fname), "file.%d", (int)getpid());
+    if (is_afs_env) {
+        snprintf(cmd, sizeof(cmd), flush_afs_cmd, fname);
+    }
+
     for (int i = 0; i < repeat; i++) {
         total_write += file_io(WRITE, fname, total_size);
+        /* here lets flush the file out of the cache */
+        if (is_afs_env) {
+            if ((status = system(cmd))) {
+                printf("'%s' FAILED...\n", cmd);
+            }
+        }
+
         total_read += file_io(READ, fname, total_size);
         remove(fname);
     }
@@ -82,6 +98,7 @@ int main(int argc, char** argv) {
     int multiplier = 1, file_size = 0, stop_bool = 0, repeat = 1;
     int64_t total_size;
     char *p_c, *p_g, c;
+    char buffer[1024], *str;
 
     if (argc < 2) {
         usage(argv[0]);
@@ -108,6 +125,14 @@ int main(int argc, char** argv) {
         printf("error: repeat value '%s' is invalid\n", argv[2]);
         usage(argv[0]);
         return -1;
+    }
+
+    // now if we are in AFS
+    str = getcwd(buffer, sizeof(buffer));
+    if (strstr(str, "/afs") == str) {
+        printf("%s\n", buffer);
+        printf("Detected AFS env\n");
+        is_afs_env = 1;
     }
 
     *p_g = '\0';
