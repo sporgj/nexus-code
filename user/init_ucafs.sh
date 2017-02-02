ceb3dbe9aa3ec70ba489779a44b5d5bb65847991
briand <jbriand@cs.pitt.edu>
2017-02-02 18:52:46 +0000

    change init_ucafs.sh to correct access rights correctly

--- user/init_ucafs.sh
+++ user/init_ucafs.sh
@@ -4,12 +4,17 @@ if [ "$#" -lt 1 ]; then
   exit 1
 fi
 
-set -x
-
 repo_dir="/afs/xyz.vm/user/$1"
+
 set -e
-echo "clearing $repo_dir/{.afsx, .ucafs, sgx}"
+echo "Creating $repo_dir/{.afsx, .ucafs, sgx}"
 rm -rf $repo_dir/.afsx $repo_dir/.ucafs $repo_dir/sgx/*
 mkdir -p $repo_dir/.afsx $repo_dir/sgx
+
+# make sure users can access the folders
+fs sa -d $repo_dir/ -a system:anyuser write
 fs sa -d $repo_dir/.afsx -a system:anyuser write
+fs sa -d $repo_dir/sgx -a system:anyuser write
+
+set -x
 ./admin_ucafs --init
