cc5f1389222f17541ba37e6a3374464e93d08769
Jack Lange <jacklange@cs.pitt.edu>
2017-12-08 22:42:53 -0500

    add missing file

---
+++ libnexus/nexus_backend.c
@@ -0,0 +1,86 @@
+/* 
+ * Copyright (c) 2017, Jack Lange <jacklange@cs.pitt.edu>
+ * All rights reserved.
+ *
+ * This is free software.  You are permitted to use,
+ * redistribute, and modify it as specified in the file "PETLAB_LICENSE".
+ */
+
+#include <stdint.h>
+
+#include <nexus_backend.h>
+
+#include <nexus_hashtable.h>
+#include <nexus_util.h>
+#include <nexus_log.h>
+
+static struct nexus_hashtable * backend_table = NULL;
+
+/*
+ * This is a place holder to ensure that the _nexus_backends section gets created by gcc
+ */
+static struct {} null_backend  __attribute__((__used__))                    \
+__attribute__((unused, __section__ ("_v3_backends"),                  \
+                   aligned(sizeof(uintptr_t))));
+
+
+
+static uint32_t
+backend_hash_fn(uintptr_t key)
+{
+    char * name = (char *)key;
+    return nexus_hash_buffer((uint8_t *)name, strlen(name));
+}
+
+
+static int
+backend_eq_fn(uintptr_t key_1,
+	      uintptr_t key_2)
+{
+    char * name_1 = (char *)key_1;
+    char * name_2 = (char *)key_2;
+    
+    return (strcasecmp(name_1, name_2) == 0);
+}
+
+
+
+int
+nexus_backend_init()
+{
+    extern struct nexus_backend_impl  * __start__nexus_backends[];
+    extern struct nexus_backend_impl  * __stop__nexus_backends[];
+    struct nexus_backend_impl        ** tmp_backend = __start__nexus_backends;
+
+    int i = 0;
+
+    backend_table = nexus_create_htable(0, backend_hash_fn, backend_eq_fn);
+
+
+    if (backend_table == NULL) {
+	log_error("Could not allocate backend table\n");
+	return -1;
+    }
+    
+
+    while (tmp_backend != __stop__nexus_backends) {
+	log_debug("Registering Backend (%s)\n", (*tmp_backend)->name);
+
+
+	if (nexus_htable_search(backend_table, (uintptr_t)((*tmp_backend)->name))) {
+	    log_error("Backend (%s) is already registered\n", (*tmp_backend)->name);
+	    return -1;
+	}
+
+
+	if (nexus_htable_insert(backend_table, (uintptr_t)((*tmp_backend)->name), (uintptr_t)(*tmp_backend)) == 0) {
+	    log_error("Could not register backend (%s)\n", (*tmp_backend)->name);
+	    return -1;
+	}
+	
+	tmp_backend = &(__start__nexus_backends[++i]);
+    }
+    
+    
+    return 0;
+}
