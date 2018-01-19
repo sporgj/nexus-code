bbd70f019e4c6d57c5aff52a8ebbeb0c0cb81f7d
Jack Lange <jacklange@cs.pitt.edu>
2018-01-19 18:28:29 -0500

    update buffer_layer API

--- backend_sgx/enclave/buffer_layer.h
+++ backend_sgx/enclave/buffer_layer.h
@@ -6,8 +6,8 @@
  * @param p_untrusted_ptr
  * @return the untrusted address
  */
-struct nexus_uuid *
-buffer_layer_alloc(size_t total_size, uint8_t ** p_untrusted_ptr);
+void *
+buffer_layer_alloc(size_t total_size, struct nexus_uuid * uuid);
 
 
 /**
@@ -16,12 +16,12 @@ buffer_layer_alloc(size_t total_size, uint8_t ** p_untrusted_ptr);
  * @param size
  * @return the uuid pointint to the nexus_buffer
  */
-struct nexus_uuid *
-buffer_layer_create(uint8_t * untrusted_addr, size_t size);
+void *
+buffer_layer_get(struct nexus_uuid * uuid, size_t * size);
 
 /**
  * Deallocates an allocated buffer
  * @param buffer_uuid
  */
 int
-buffer_layer_free(struct nexus_uuid * buffer_uuid);
+buffer_layer_put(struct nexus_uuid * buffer_uuid);
