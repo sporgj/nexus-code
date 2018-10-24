9835c3de7970088a32ea46eb7552f8b5ed219878
Briand <jbriand@cs.pitt.edu>
2018-10-24 14:36:36 -0400

    adding nexus_list

---
+++ libnexus/nexus_list.c
@@ -0,0 +1,195 @@
+#include <string.h>
+
+#include "nexus_list.h"
+#include "nexus_util.h"
+
+struct list_node {
+    void * data;
+
+    struct list_head node;
+};
+
+struct nexus_list_iterator {
+    struct nexus_list * list;
+    struct list_head  * curr;
+};
+
+static void
+free_list_node(struct nexus_list * list, struct list_node * node)
+{
+    if (list->attrs.deallocator != NULL) {
+        list->attrs.deallocator(node->data);
+    }
+
+    list_del(&node->node);
+
+    nexus_free(node);
+}
+
+void
+nexus_list_init(struct nexus_list * list)
+{
+    memset(list, 0, sizeof(struct nexus_list));
+
+    INIT_LIST_HEAD(&(list->list));
+}
+
+void
+nexus_list_destroy(struct nexus_list * list)
+{
+    while (!list_empty(&list->list)) {
+        struct list_node * node = list_first_entry(&list->list, struct list_node, node);
+
+        free_list_node(list, node);
+    }
+}
+
+void *
+nexus_list_get(struct nexus_list * list, size_t pos) {
+    struct list_head * head = &list->list;
+    struct list_head * curr = head->next;
+    struct list_node * node = NULL;
+
+    for (size_t i = 0; i < pos; i++) {
+        if (curr == head) {
+            return NULL;
+        }
+
+        curr = curr->next;
+    }
+
+    if (curr == head) {
+      return NULL;
+    }
+
+    node = list_entry(curr, struct list_node, node);
+
+    return node->data;
+}
+
+void *
+nexus_list_pop(struct nexus_list * list)
+{
+    void * result = NULL;
+
+    struct list_node * last_node = NULL;
+
+    struct list_head * list_head = &list->list;
+
+    if (list_empty(list_head)) {
+        return NULL;
+    }
+
+
+    last_node = list_last_entry(list_head, struct list_node, node);
+    list_del(&last_node->node);
+
+    result = last_node->data;
+
+    nexus_free(last_node);
+
+    return result;
+}
+
+void
+nexus_list_set_deallocator(struct nexus_list * list, list_deallocator deallocator)
+{
+    list->attrs.deallocator = deallocator;
+}
+
+int
+nexus_list_prepend(struct nexus_list * list, void * element)
+{
+    struct list_node * node = NULL;
+
+    node = nexus_malloc(sizeof(struct list_node));
+
+    node->data = element;
+
+    list_add(&node->node, &list->list);
+
+    return 0;
+}
+
+int
+nexus_list_append(struct nexus_list * list, void * element)
+{
+    struct list_node * node = NULL;
+
+    node = nexus_malloc(sizeof(struct list_node));
+
+    node->data = element;
+
+    list_add_tail(&node->node, &list->list);
+
+    return 0;
+}
+
+struct nexus_list_iterator *
+list_iterator_new(struct nexus_list * list)
+{
+    struct nexus_list_iterator * iter = NULL;
+
+    iter = nexus_malloc(sizeof(struct nexus_list_iterator));
+
+    iter->list = list;
+    iter->curr = list->list.next;
+
+    return iter;
+}
+
+void *
+list_iterator_get(struct nexus_list_iterator * iter)
+{
+    struct list_node * node = NULL;
+
+    if (iter->curr == &iter->list->list) {
+        return NULL;
+    }
+
+    node = list_entry(iter->curr, struct list_node, node);
+
+    return node->data;
+}
+
+bool
+list_iterator_is_valid(struct nexus_list_iterator * iter)
+{
+    if (iter->curr == &iter->list->list) {
+        return false;
+    }
+
+    return true;
+}
+
+void
+list_iterator_next(struct nexus_list_iterator * iter)
+{
+    if (iter->curr != &iter->list->list) {
+        iter->curr = iter->curr->next;
+    }
+}
+
+int
+list_iterator_del(struct nexus_list_iterator * iter)
+{
+    struct list_node * node = NULL;
+
+    if (iter->curr == &iter->list->list) {
+        return -1;
+    }
+
+    node = list_entry(iter->curr, struct list_node, node);
+
+    iter->curr = iter->curr->next;
+
+    free_list_node(iter->list, node);
+
+    return 0;
+}
+
+void
+list_iterator_free(struct nexus_list_iterator * iter)
+{
+    free(iter);
+}
---
+++ libnexus/nexus_list.h
@@ -0,0 +1,133 @@
+/**
+ * Copyright (C) 2018 Judicael Djoko <sporgj@gmail.com>
+ *
+ * This file leverages list_head defined in list.h to provide a simplified interface
+ * to a list. Features such as multithreading, sorting etc. will be added.
+ *
+ * This is free software. You are permitted to use, redistribute, and modify it.
+ */
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "list.h"
+
+typedef void (*list_deallocator)(void * element);
+
+struct nexus_list_iterator;
+
+struct nexus_list {
+    struct list_head list;
+
+    struct {
+        list_deallocator deallocator;
+    } attrs;
+};
+
+/**
+ * Initializes an empty list
+ * @param list
+ */
+void
+nexus_list_init(struct nexus_list * list);
+
+/**
+ * Frees all the contents of the list
+ * @param list
+ */
+void
+nexus_list_destroy(struct nexus_list * list);
+
+/**
+ * Gets an element at a specific position in the list
+ * @param list
+ * @param po
+ * @return the data
+ */
+void *
+nexus_list_get(struct nexus_list * list, size_t pos);
+
+/**
+ * Removes the last element from list and returns to caller. It is the caller's responsibility
+ * to free the returned element.
+ *
+ * @param list
+ * @return element, NULL if is empty
+ */
+void *
+nexus_list_pop(struct nexus_list * list);
+
+/**
+ * Sets the deallocator function of the list. Called whenever an element is removed
+ * @param list
+ * @param deallocator
+ */
+void
+nexus_list_set_deallocator(struct nexus_list * list, list_deallocator deallocator);
+
+/**
+ * Prepends element to the list
+ * @param list
+ * @param element
+ * @return 0 on success
+ */
+int
+nexus_list_prepend(struct nexus_list * list, void * element);
+
+/**
+ * Appends an element to the list
+ * @param list
+ * @param element
+ * @return 0 on success
+ */
+int
+nexus_list_append(struct nexus_list * list, void * element);
+
+
+/**
+ * Creates a new iterator and points to the first element in the list
+ * @param list
+ * @return struct nexus_list_iterator
+ */
+struct nexus_list_iterator *
+list_iterator_new(struct nexus_list * list);
+
+/**
+ * Checks if the iterator points to a valid element
+ * @param iter
+ * @return bool
+ */
+bool
+list_iterator_is_valid(struct nexus_list_iterator * iter);
+
+/**
+ * Returns the element currently pointed by the iterator
+ * @param iter
+ * @return data
+ */
+void *
+list_iterator_get(struct nexus_list_iterator * iter);
+
+/**
+ * Moves the iterator to the next
+ * @param iter
+ */
+void
+list_iterator_next(struct nexus_list_iterator * iter);
+
+/**
+ * Deletes the item currently pointed by the element. Iterator moves to the
+ * next
+ * @param iter
+ * @return 0 on success
+ */
+int
+list_iterator_del(struct nexus_list_iterator * iter);
+
+/**
+ * Frees the allocated iterator
+ * @param iter
+ */
+void
+list_iterator_free(struct nexus_list_iterator * iter);
