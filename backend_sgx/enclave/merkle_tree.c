301ea50c2a0da27070dec01b8bea9eecaff6faf7
Pranut Jain <pxj141030@utdallas.edu>
2018-12-06 19:48:05 -0500

    Updated Metadata.c to also load macs before storing. Refactored merkle init method.

--- backend_sgx/enclave/merkle_tree.c
+++ backend_sgx/enclave/merkle_tree.c
@@ -13,38 +13,41 @@ struct nexus_mac * root_mac;
  * Initializes the merkle hash tree, by loading
  * the hash file
  */
-//int
-//merkle_init(nexus_uuid *vol_ptr) {
-//
-//    if (vol_ptr != NULL) {
-//        int size = 0;
-//        void *d_ptr;
-//        
-//        //making an ocall to get the hash of the root node
-//        if (ocall_merkle_get(vol_ptr, d_ptr, &size)) {
-//            log_error("ocall_stash_get FAILED\n");
-//            return -1;
-//        }
-//        
-//        if(d_ptr == NULL) {
-//            log_error("ocall_merkle_get FAILED d_ptr issue");
-//            return -1;
-//        }
-//        
-//        root_mac = nexus_malloc(sizeof (struct nexus_mac));
-//        
-//        if (root_mac == NULL) {
-//            log_error("root_mac malloc FAILED\n");
-//            return -1;
-//        }
-//                
-//        memcpy(root_mac, d_ptr, size);
-//    } else {
-//        //need volume info
-//        return -1;
-//    }
-//    return 0;
-//}
+int
+merkle_init(nexus_uuid *vol_ptr) {
+
+    if (vol_ptr != NULL) {
+        
+        root_mac = nexus_malloc(sizeof (struct nexus_mac));
+        
+        //making an ocall to load vol merkle file
+        int err = -1;
+        int ret = -1;
+
+        err = ocall_merkle_init(&ret, vol_ptr, root_mac);
+
+        if (err || ret) {
+            log_error("merkle_init FAILED (err=%d, ret=%d)\n", err, ret);
+            return -1;
+        }   
+        
+        if(d_ptr == NULL) {
+            log_error("ocall_merkle_get FAILED d_ptr issue");
+            return -1;
+        }
+        
+        if (root_mac == NULL) {
+            log_error("root_mac malloc FAILED\n");
+            return -1;
+        }
+                
+    } else {
+        //need volume info
+        log_error("merkle_init FAILED\n");
+        return -1;
+    }
+    return 0;
+}
 
 /* 
  * Updates mac value of the modified node in the
