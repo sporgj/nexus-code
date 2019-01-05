b4d8c296ee51b03dbd1c88f834609133cab4e085
Briand <jbriand@cs.pitt.edu>
2019-01-05 13:13:29 -0500

    removed stupid code

--- libnexus/nexus_main.c
+++ libnexus/nexus_main.c
@@ -20,42 +20,6 @@
 
 
 
-#if 0
-int
-nexus_dirnode_lookup(struct dirnode *      dirnode,
-                     char *                fname,
-                     struct uuid *         uuid_dest,
-                     nexus_fs_obj_type_t * p_type)
-{
-    return backend_dirnode_find_by_name(dirnode, fname, uuid_dest, p_type);
-}
-
-void *
-nexus_generate_metadata(struct nexus_volume * volume,
-                        struct uuid *         uuid,
-                        nexus_fs_obj_type_t   type)
-{
-    struct dirnode * dirnode = NULL;
-
-    struct uuid * root_uuid = &volume->supernode->header.root_uuid;
-
-    int ret = -1;
-
-    if (type == NEXUS_DIR) {
-        ret = backend_dirnode_new(uuid, root_uuid, &dirnode);
-        if (ret != 0) {
-            log_error("backend_dirnode_new() FAILED");
-            return NULL;
-        }
-
-        return dirnode;
-    }
-
-    return NULL;
-}
-#endif
-
-
 int
 nexus_init()
 {
@@ -65,7 +29,6 @@ nexus_init()
     nexus_config_init();
     nexus_backend_init();
     nexus_datastores_init();
-    
 
     return 0;
 }
@@ -83,62 +46,3 @@ nexus_setup()
 {
     return nexus_create_user_data();
 }
-
-
-
-
-#if 0
-
-int
-nexus_create_volume(const char * metadata_dirpath,
-                    const char * publickey_fpath,
-                    const char * volumekey_fpath)
-{
-    struct supernode supernode;
-    struct dirnode   root_dirnode;
-    struct volumekey volumekey;
-
-    struct uuid supernode_uuid;
-    struct uuid root_uuid;
-
-    FILE * fd = NULL;
-
-    int ret = -1;
-
-    nexus_uuid(&supernode_uuid);
-    nexus_uuid(&root_uuid);
-
-    ret = backend_volume_create(&supernode_uuid,
-                                &root_uuid,
-                                publickey_fpath,
-                                &supernode,
-                                &root_dirnode,
-                                &volumekey);
-
-    if (ret != 0) {
-        log_error("backend_volume_create FAILED ret=%d", ret);
-        goto out;
-    }
-
-    ret = metadata_create_volume(&supernode,
-                                 &root_dirnode,
-                                 &volumekey,
-                                 metadata_dirpath,
-                                 volumekey_fpath);
-
-    if (ret != 0) {
-        log_error("metadata_create_volume() FAILED");
-        goto out;
-    }
-
-    ret = 0;
-out:
-    if (fd) {
-        fclose(fd);
-    }
-
-    return ret;
-}
-
-
-#endif
