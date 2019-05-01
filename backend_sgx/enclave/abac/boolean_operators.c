097cfa812b84315a71900fcd6209bbfe158e1a6a
Briand <jbriand@cs.pitt.edu>
2019-05-01 03:57:47 -0400

    removing boolean operator execution

--- backend_sgx/enclave/abac/boolean_operators.c
+++ backend_sgx/enclave/abac/boolean_operators.c
@@ -6,99 +6,17 @@
 struct __bool_op {
     char * name;
     char * datalog_str;
-    int    (*handler)(struct abac_value * arg1, struct abac_value * arg2, bool * result_bool);
 };
 
 
-
-static int
-__handle_greater_than(struct abac_value * arg1, struct abac_value * arg2, bool * result_bool)
-{
-    if (arg1->type == ABAC_VALUE_NUMBER && arg2->type == ABAC_VALUE_NUMBER) {
-        *result_bool = (arg1->int_val > arg2->int_val);
-        return 0;
-    } else if (arg1->type == ABAC_VALUE_STRING && arg2->type == ABAC_VALUE_STRING) {
-        *result_bool = (strncmp(arg1->str_val, arg2->str_val, arg1->data_sz) > 0);
-        return 0;
-    }
-
-    log_error("mimatched data types\n");
-
-    return -1;
-}
-
-static int
-__handle_lesser_than(struct abac_value * arg1, struct abac_value * arg2, bool * result_bool)
-{
-    if (arg1->type == ABAC_VALUE_NUMBER && arg2->type == ABAC_VALUE_NUMBER) {
-        *result_bool = (arg1->int_val < arg2->int_val);
-        return 0;
-    } else if (arg1->type == ABAC_VALUE_STRING && arg2->type == ABAC_VALUE_STRING) {
-        *result_bool = (strncmp(arg1->str_val, arg2->str_val, arg1->data_sz) < 0);
-        return 0;
-    }
-
-    log_error("mimatched data types\n");
-
-    return -1;
-}
-
-static int
-__handle_greater_or_equals(struct abac_value * arg1, struct abac_value * arg2, bool * result_bool)
-{
-    if (arg1->type == ABAC_VALUE_NUMBER && arg2->type == ABAC_VALUE_NUMBER) {
-        *result_bool = (arg1->int_val >= arg2->int_val);
-        return 0;
-    } else if (arg1->type == ABAC_VALUE_STRING && arg2->type == ABAC_VALUE_STRING) {
-        *result_bool = (strncmp(arg1->str_val, arg2->str_val, arg1->data_sz) >= 0);
-        return 0;
-    }
-
-    log_error("mimatched data types\n");
-
-    return -1;
-}
-
-static int
-__handle_lesser_or_equals(struct abac_value * arg1, struct abac_value * arg2, bool * result_bool)
-{
-    if (arg1->type == ABAC_VALUE_NUMBER && arg2->type == ABAC_VALUE_NUMBER) {
-        *result_bool = (arg1->int_val <= arg2->int_val);
-        return 0;
-    } else if (arg1->type == ABAC_VALUE_STRING && arg2->type == ABAC_VALUE_STRING) {
-        *result_bool = (strncmp(arg1->str_val, arg2->str_val, arg1->data_sz) <= 0);
-        return 0;
-    }
-
-    log_error("mimatched data types\n");
-
-    return -1;
-}
-
-static int
-__handle_double_equals(struct abac_value * arg1, struct abac_value * arg2, bool * result_bool)
-{
-    if (arg1->type == ABAC_VALUE_NUMBER && arg2->type == ABAC_VALUE_NUMBER) {
-        *result_bool = (arg1->int_val == arg2->int_val);
-        return 0;
-    } else if (arg1->type == ABAC_VALUE_STRING && arg2->type == ABAC_VALUE_STRING) {
-        *result_bool = (strncmp(arg1->str_val, arg2->str_val, arg1->data_sz) == 0);
-        return 0;
-    }
-
-    log_error("mismatched data types\n");
-
-    return -1;
-}
-
 static struct __bool_op __boolean_operators[] = {
-    { ">",  "_gt", __handle_greater_than },
-    { "<",  "_lt", __handle_lesser_than },
-    { ">=", "_ge", __handle_greater_or_equals },
-    { "<=", "_le", __handle_lesser_or_equals },
-    { "!=", "_ne", __handle_lesser_or_equals },
-    { "==", "=", __handle_double_equals },
-    { NULL,  NULL, NULL },
+    { ">",  "_gt" },
+    { "<",  "_lt" },
+    { ">=", "_ge" },
+    { "<=", "_le" },
+    { "!=", "_ne" },
+    { "==", "=",  },
+    { NULL,  NULL },
 };
 
 static struct __bool_op *
@@ -139,20 +57,4 @@ boolean_operator_exists(char * boolean_str)
     }
 
     return true;
-}
-
-int
-boolean_operator_execute(char              * boolean_str,
-                         struct abac_value * arg1,
-                         struct abac_value * arg2,
-                         bool              * result)
-{
-    struct __bool_op * bool_operator = __find_boolean_operator(boolean_str);
-
-    if (bool_operator == NULL) {
-        log_error("could not find `%s` boolean operator\n", boolean_str);
-        return NULL;
-    }
-
-    return bool_operator->handler(arg1, arg2, result);
 }
--- backend_sgx/enclave/abac/boolean_operators.h
+++ backend_sgx/enclave/abac/boolean_operators.h
@@ -2,15 +2,6 @@
 
 #include <stdbool.h>
 
-#include "value.h"
-
-
-int
-boolean_operator_execute(char              * boolean_str,
-                         struct abac_value * arg1,
-                         struct abac_value * arg2,
-                         bool              * result);
-
 const char *
 boolean_operator_to_datalog_str(char * boolean_str);
 
